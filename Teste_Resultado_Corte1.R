2de546df00cb213206572b20722549386c225c47
OMANID01 <66449407+OMANID01@users.noreply.github.com>
2024-07-03 17:27:58 -0300

    Add files via upload

---
+++ Teste_Resultado_Corte1.R
@@ -0,0 +1,27 @@
+#LEITURA DE DADOS 
+dados<-read.table("dados.txt",header = T,dec=",")
+
+str(dados)
+dados$cv<-as.factor(dados$cv)
+
+dados$rep<-as.factor(dados$rep)
+
+dados$corte<-as.factor(dados$corte)
+
+#ANEXAR OS DADOS 
+attach(dados)
+names(dados)
+
+#RESUMO ESTATISTICO
+summary(dados)
+
+#ATIVAR PACOTE ExpDes.pt
+library(ExpDes.pt)
+
+#RODAR OS DADOS
+dic(dados$cv,dados$ALT,mcomp="tukey")
+dic(dados$cv,dados$MSA,mcomp="tukey")
+dic(dados$cv,dados$MSCP,mcomp="tukey")
+dic(dados$cv,dados$PSA,mcomp="tukey")
+dic(dados$cv,dados$PSCP,mcomp="tukey")
+dic(dados$cv,dados$PMST,mcomp="tukey")
